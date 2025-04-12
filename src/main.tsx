3feadf34788f0b8989d35ff972e373e7e42937f8
wesleyshun2 <wesleyshun2@gmail.com>
2025-04-12 19:27:57 +0800

    add testnet

--- src/main.tsx
+++ src/main.tsx
@@ -12,13 +12,14 @@ const queryClient = new QueryClient();
 const networks = {
     devnet: { url: getFullnodeUrl('devnet') },
     mainnet: { url: getFullnodeUrl('mainnet') },
+    testnet: { url: getFullnodeUrl('testnet') },
 };
 
 // 渲染應用
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
         <QueryClientProvider client={queryClient}>
-            <SuiClientProvider networks={networks} defaultNetwork="devnet">
+            <SuiClientProvider networks={networks} defaultNetwork="testnet">
                 <WalletProvider>
                     <App />
                 </WalletProvider>
